331d5ed635dc7a96e71d4db81e70a699234bd787
jose-luis-rs <joseluis.rodriguez.sanchez@usc.es>
2024-12-09 23:23:32 +0100

    Moved old folder githubfiles and removed old github configurations to run the CI tests

--- .githubfiles/run_build_test_apr22.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf /tmp/r3broot
-
-./Dart.sh Experimental .githubfiles/dart_experimental_apr22.cfg
-
-exit $?
-
--- .githubfiles/run_build_test_jun19p2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf /tmp/r3broot
-
-./Dart.sh Experimental .githubfiles/dart_experimental_jun19p2.cfg
-
-exit $?
-
