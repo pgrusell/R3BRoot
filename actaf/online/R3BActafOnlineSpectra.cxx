ba8fb1cc46f8cca70993cf0b47fc7e5e0fc67efa
jose-luis-rs <j.l.rodriguez.sanchez@udc.es>
2026-04-08 18:37:18 +0200

    Added plots for gas quality

--- actaf/online/R3BActafOnlineSpectra.cxx
+++ actaf/online/R3BActafOnlineSpectra.cxx
@@ -216,7 +216,7 @@ InitStatus R3BActafOnlineSpectra::Init()
     for (int index = 0; index < 3; index++)
     {
      cGas->cd(index+1);
-     fh2_gasquality.push_back(R3B::root_owned<TH2F>(Form("fh2_gasquality_%d",index+1), alpha_source[i].Data(), 200, 0, 40000, 500, -50000, 250000));
+     fh2_gasquality.push_back(R3B::root_owned<TH2F>(Form("fh2_gasquality_%d",index+1), alpha_source[index].Data(), 200, 0, 40000, 500, -50000, 250000));
      fh2_gasquality[index]->GetXaxis()->SetTitle("Event number");
      fh2_gasquality[index]->GetYaxis()->SetTitle("Integral [ADC chn]");
      fh2_gasquality[index]->GetYaxis()->SetTitleOffset(1.);
