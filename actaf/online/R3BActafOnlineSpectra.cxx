c914354111713e36799b373f4348d080d232e05d
jose-luis-rs <j.l.rodriguez.sanchez@udc.es>
2026-04-08 16:38:39 +0200

    Changed histrogram shapes

--- actaf/online/R3BActafOnlineSpectra.cxx
+++ actaf/online/R3BActafOnlineSpectra.cxx
@@ -163,8 +163,8 @@ InitStatus R3BActafOnlineSpectra::Init()
     fh2_Baseline_map->GetXaxis()->SetTitle("Pad");
     fh2_Baseline_map->GetYaxis()->SetTitle("Baseline [ADC chn]");
     fh2_Baseline_map->GetYaxis()->SetTitleOffset(1.1);
-    fh2_Baseline_map->GetYaxis()->SetMaxDigits(3);
-    fh2_Baseline_map->GetYaxis()->SetNoExponent(kFALSE);
+    fh2_Baseline_map->GetYaxis()->SetMaxDigits(2);
+    // fh2_Baseline_map->GetYaxis()->SetNoExponent(kFALSE);
     fh2_Baseline_map->GetXaxis()->CenterTitle(true);
     fh2_Baseline_map->GetYaxis()->CenterTitle(true);
     fh2_Baseline_map->Draw("colz");
