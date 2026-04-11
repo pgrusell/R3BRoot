a58544ab22b27372919e8b63d05299ace5eeb761
jose-luis-rs <j.l.rodriguez.sanchez@udc.es>
2026-04-11 12:44:06 +0200

    Change binning

--- actaf/online/R3BActafOnlineSpectra.cxx
+++ actaf/online/R3BActafOnlineSpectra.cxx
@@ -227,7 +227,7 @@ InitStatus R3BActafOnlineSpectra::Init()
     {
         cGas->cd(index + 1);
         fh2_gasquality.push_back(R3B::root_owned<TH2F>(
-            Form("fh2_gasquality_%d", index + 1), alpha_source[index].Data(), 200, 0, 400000, 500, -50000, 250000));
+            Form("fh2_gasquality_%d", index + 1), alpha_source[index].Data(), 200, 0, 400000, 5000, -50000, 250000));
         fh2_gasquality[index]->GetXaxis()->SetTitle("Event number");
         fh2_gasquality[index]->GetYaxis()->SetTitle("Integral [ADC chn]");
         fh2_gasquality[index]->GetYaxis()->SetTitleOffset(1.);
