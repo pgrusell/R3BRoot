119c1cf89b2c0e6ec9ef5d05a370479f427be411
jose-luis-rs <j.l.rodriguez.sanchez@udc.es>
2026-04-25 22:34:08 +0200

    New approach

--- actaf/online/R3BActafOnlineSpectra.cxx
+++ actaf/online/R3BActafOnlineSpectra.cxx
@@ -1115,8 +1115,8 @@ InitStatus R3BActafOnlineSpectra::Init()
         fh1_Cluster_energy[0]->Draw();
         
         cClusterUp->cd(6);
-        fh2_XYPos_clusters.push_back(R3B::root_owned<TH2Poly>(fh2_XYPos[i]->Clone((fh2_XYPos[i]->GetTitle() + TString("_clusters)")).Data()));
-        fh2_XYPos_clusters[0]->SetTitle(fh2_XYPos[i]->GetTitle() + TString(" per event"));
+        fh2_XYPos_clusters.push_back(R3B::root_owned<TH2Poly>(fh2_XYPos[0]->Clone((fh2_XYPos[0]->GetTitle() + TString("_clusters)")).Data()));
+        fh2_XYPos_clusters[0]->SetTitle(fh2_XYPos[0]->GetTitle() + TString(" per event"));
         fh2_XYPos_clusters[0]->SetLineColor(kBlack);
         fh2_XYPos_clusters[0]->SetLineWidth(1);
         fh2_XYPos_clusters[0]->Draw("colz ]");
@@ -1180,8 +1180,8 @@ InitStatus R3BActafOnlineSpectra::Init()
         
         cClusterDown->cd(6);
  
-        fh2_XYPos_clusters.push_back(R3B::root_owned<TH2Poly>(fh2_XYPos[i]->Clone((fh2_XYPos[i]->GetTitle() + TString("_clusters)")).Data()));
-        fh2_XYPos_clusters[1]->SetTitle(fh2_XYPos[i]->GetTitle() + TString(" per event"));
+        fh2_XYPos_clusters.push_back(R3B::root_owned<TH2Poly>(fh2_XYPos[1]->Clone((fh2_XYPos[1]->GetTitle() + TString("_clusters)")).Data()));
+        fh2_XYPos_clusters[1]->SetTitle(fh2_XYPos[1]->GetTitle() + TString(" per event"));
         fh2_XYPos_clusters[1]->SetLineColor(kBlack);
         fh2_XYPos_clusters[1]->SetLineWidth(1);
         fh2_XYPos_clusters[1]->Draw("colz ]");
